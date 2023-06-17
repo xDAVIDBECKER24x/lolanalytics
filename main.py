d967cccefffca6e691b4c7ca6b912cc32f0a239e
David <drcarollo123@gmail.com>
2023-06-17 03:13:40 -0300

    test

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import requests
 
 username = 'Beckynho'
-
 api_key = "RGAPI-c29ff9d4-7749-47e3-846b-b161d1eb25eb"
-url = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Beckynho?api_key=RGAPI-c29ff9d4-7749-47e3-846b-b161d1eb25eb'
-# headers = {'X-Riot-Token': api_key}
 
-res = requests.get(url)
-user = res.json()
-id = user['id']
-print(id)
+def get_player_data_by_name(name):
+
+    url = f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}?api_key={api_key}"
+    
+    return requests.get(url).json()
+
+
+def get_match_data_by_player_id(id):
+
+    url = f"https://br1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{id}?api_key={api_key}"
+
+    return  requests.get(url).json()
+
+
+
+# sLVdO1TGer3qzYDbxSeBCo-VHsxDU95DxPOrQowp8l12Aw
+
+player = get_player_data_by_name(username)
+match = get_match_data_by_player_id(player['id'])
+# user = res.json()
+# id = user['id']
+print(match)

bdbf2437b8f2947c8b8a89cabc7ded60381bbd44
David <drcarollo123@gmail.com>
2023-07-04 17:05:35 -0300

    json format

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ def format_json(json_data):
 
     return json_formatted
 
-
 def check_game_mode(match, mode):
 
     if (match['info']['gameMode'] in mode):
         return True
 
     return False
-
 
 def check_game_type(match, type):
 
@@ -27,7 +25,6 @@ def check_game_type(match, type):
 
     return False
 
-
 def check_match_settings(match, match_settings):
 
     if ((check_game_type(match, match_settings['game_type']) & check_game_mode(match, match_settings['game_mode'])) == True):
@@ -35,7 +32,6 @@ def check_match_settings(match, match_settings):
         return True
 
     return False
-
 
 def get_player_match_info_by_player_puuid(match, puuid):
 
@@ -47,7 +43,6 @@ def get_player_match_info_by_player_puuid(match, puuid):
     player_match_info = match['info']['participants'][player_idx]
 
     return player_match_info
-
 
 def save_match_overview(match_list, puuid, save_file):
 
@@ -77,47 +72,52 @@ def save_match_overview(match_list, puuid, save_file):
 
 
 def save_pings_overview(match_list, match_settings, puuid, save_file):
+    
+    geral_pings_overview = []
+
+    for match in match_list:
+
+        if (check_match_settings(match, match_settings) == True):
+
+            pings_overview = {}
+
+            player_data = get_player_match_info_by_player_puuid(match, puuid)
+
+            match_duration = match['info']['gameDuration']
+            match_duration = strftime("%H:%M:%S", gmtime(match_duration))
+
+            # Convert miliseconds timestamp to seconds
+            match_creation = int(match['info']['gameCreation']/1000)
+            match_creation = datetime.utcfromtimestamp(
+                match_creation).strftime('%d-%m-%Y')
+
+            print(match_creation)
+            pings_overview['gameCreation'] = match_creation
+            pings_overview['gameDuration'] = match_duration
+            pings_overview['championName'] = player_data['championName']
+            pings_overview['win'] = player_data['win']
+            pings_overview['allInPings'] = player_data['allInPings']
+            pings_overview['baitPings'] = player_data['baitPings']
+            pings_overview['basicPings'] = player_data['basicPings']
+            pings_overview['commandPings'] = player_data['commandPings']
+            pings_overview['dangerPings'] = player_data['dangerPings']
+            pings_overview['enemyMissingPings'] = player_data['enemyMissingPings']
+            pings_overview['enemyVisionPings'] = player_data['enemyVisionPings']
+            pings_overview['getBackPings'] = player_data['getBackPings']
+            pings_overview['holdPings'] = player_data['holdPings']
+            pings_overview['needVisionPings'] = player_data['needVisionPings']
+            pings_overview['onMyWayPings'] = player_data['onMyWayPings']
+            pings_overview['pushPings'] = player_data['pushPings']
+
+            geral_pings_overview.append(pings_overview)
 
     with open(save_file, "w") as outfile:
+        
+        geral_pings_overview = format_json(geral_pings_overview)
+        outfile.write(geral_pings_overview)
 
-        for match in match_list:
-
-            if (check_match_settings(match, match_settings) == True):
-
-                pings_overview = {}
-
-                player_data = get_player_match_info_by_player_puuid(
-                    match, puuid)
-
-                match_duration = match['info']['gameDuration']
-                match_duration = strftime("%H:%M:%S", gmtime(match_duration))
-
-                # Convert miliseconds timestamp to seconds
-                match_creation = int(match['info']['gameCreation']/1000)
-                match_creation = datetime.utcfromtimestamp(
-                    match_creation).strftime('%d-%m-%Y')
-
-                print(match_creation)
-                pings_overview['gameCreation'] = match_creation
-                pings_overview['gameDuration'] = match_duration
-                pings_overview['championName'] = player_data['championName']
-                pings_overview['win'] = player_data['win']
-                pings_overview['allInPings'] = player_data['allInPings']
-                pings_overview['baitPings'] = player_data['baitPings']
-                pings_overview['basicPings'] = player_data['basicPings']
-                pings_overview['commandPings'] = player_data['commandPings']
-                pings_overview['dangerPings'] = player_data['dangerPings']
-                pings_overview['enemyMissingPings'] = player_data['enemyMissingPings']
-                pings_overview['enemyVisionPings'] = player_data['enemyVisionPings']
-                pings_overview['getBackPings'] = player_data['getBackPings']
-                pings_overview['holdPings'] = player_data['holdPings']
-                pings_overview['needVisionPings'] = player_data['needVisionPings']
-                pings_overview['onMyWayPings'] = player_data['onMyWayPings']
-                pings_overview['pushPings'] = player_data['pushPings']
-
-                pings_overview = format_json(pings_overview)
-
-                outfile.write(pings_overview)
+    # print(geral_pings_overview)          
+    # outfile.write(geral_pings_overview)
 
     return
 
